ff42ea61b15938c037cdd6465e3d6d32000570e2
Courselauncher <amberagrawal1994@gmail.com>
2024-09-10 02:03:22 +0530

    m

---
+++ js/footer-latest.js
@@ -0,0 +1,323 @@
+document.write('<div class="footerregion">\n'),
+    document.write('\x3c!--<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" data-toggle="tooltip" data-placement="left" data-original-title="" title="" style="display: inline;">\n'),
+    document.write('<span class="glyphicon glyphicon-chevron-up"></span>\n'),
+    document.write("</a>--\x3e\n"),
+    document.write("\n"),
+    document.write('<section class="backtotop text-center">\n'),
+    document.write('<div class="container">\n'),
+    document.write('<div class="row">\n'),
+    document.write('<div class="col-xs-12">\n'),
+    document.write('<a href="#head" class="smothscroll">Back to Top</a>\n'),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</section>\n"),
+    document.write("\n"),
+    document.write('<section class="footerlinksre">\n'),
+    document.write('<div class="container">\n'),
+    document.write('<div class="row">\n'),
+    document.write('<div class="col-xs-6 col-sm-3 col-md-2">\n'),
+    document.write("<h2>Study with us</h2>\n"),
+    document.write("\n"),
+    document.write('<div class="panel-group whitebgcon col-xs-12" id="accordion2" role="tablist" aria-multiselectable="true">\n'),
+    document.write('<div class="panel panel-default">\n'),
+    document.write('<div class="panel-heading" role="tab" id="headingsix">\n'),
+    document.write('<a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapsesix" aria-expanded="true" aria-controls="collapsesix" class="collapsed">After-School</a>\n'),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div id="collapsesix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingsix">\n'),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/engineering/">JEE</a></li>\n'),
+    document.write('<li><a href="/medical/ ">NEET</a></li>\n'),
+    document.write('<li><a href="/law/ ">LAW</a></li>\n'),
+    document.write('<li><a href="/cucet/">CUET</a></li>\n'),
+    document.write('<li><a href="/after12/ ">BBA-HM</a></li>\n'),
+    document.write('<li><a href="/ipm/ ">IPM</a></li>\n'),
+    document.write('<li><a href="/cbse-ncert/">Tuition</a></li>\n'),
+    document.write('<li><a href="/counselling/">Career Counseling</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="panel panel-default">\n'),
+    document.write('<div class="panel-heading" role="tab" id="headingseven">\n'),
+    document.write('<a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapseseven" aria-expanded="true" aria-controls="collapseseven" class="collapsed">After-College</a>\n'),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div id="collapseseven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingseven">\n'),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/cat-mba/">MBA</a></li>\n'),
+    document.write('<li><a href="/gate/">GATE</a></li>\n'),
+    // document.write('<li><a href="/upsc/">Civils</a></li>\n'),
+    document.write('<li><a href="/ssc/">SSC</a></li>\n'),
+    document.write('<li><a href="/banking/">Banking</a></li>\n'),
+    // document.write('<li><a href="/rbi-grade-b/">RBI</a></li>\n'),
+    document.write('<li><a href="/crt/">Placements</a></li>\n'),
+    document.write('<li><a href="/iGuru/">Interviews</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="panel panel-default">\n'),
+    document.write('<div class="panel-heading" role="tab" id="headingeight">\n'),
+    document.write('<a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapseeight" aria-expanded="true" aria-controls="collapseeight" class="collapsed">Study-Abroad</a>\n'),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div id="collapseeight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingeight">\n'),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/gmat/">GMAT</a></li>\n'),
+    document.write('<li><a href="/gre/">GRE</a></li>\n'),
+    document.write('<li><a href="/sat/">SAT</a></li>\n'),
+    document.write('<li><a href="/ielts/">IELTS</a></li>\n'),
+    // document.write('<li class="fulwidthbtn"><a href="/igcse/maths-tuition/">IGCSE Maths Tuitions</a></li>\n'),
+    document.write('<li class="fulwidthbtn"><a href="/admission-consulting/">Admission Consulting</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="panel panel-default">\n'),
+    document.write('<div class="panel-heading" role="tab" id="headingnine">\n'),
+    document.write('<a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapsenine" aria-expanded="true" aria-controls="collapsenine" class="collapsed">Upskilling</a>\n'),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div id="collapsenine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingnine">\n'),
+    document.write("<ul>\n"),
+    document.write('<li class="fulwidthbtn"><a href="/finschool/">FinSchool</a></li>\n'),
+    document.write('<li class="fulwidthbtn"><a href="/machine-learning/">DataSchool</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+
+    document.write('<div class="panel panel-default">\n'),
+    document.write('<div class="panel-heading" role="tab" id="headingten">\n'),
+    document.write('<a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapseten" aria-expanded="true" aria-controls="collapseten" class="collapsed">Webinar</a>\n'),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div id="collapseten" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingten">\n'),
+    document.write("<ul>\n"),
+    document.write('<li class="fulwidthbtn"><a href="/webinar/">Exam Prep</a></li>\n'),
+    document.write('<li class="fulwidthbtn"><a href="/webinar/tuitions/">Tuitions & Boards</a></li>\n'),
+    document.write('<li class="fulwidthbtn"><a href="/webinar/ug/">Under-Graduate</a></li>\n'),
+    document.write('<li class="fulwidthbtn"><a href="/webinar/pg/">Post-Graduate</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-6 col-sm-3 col-md-3">\n'),
+    document.write("<h2>Reach Us</h2>\n"),
+    document.write("\n"),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/contactus/head-office.html">Corporate Contacts</a></li>\n'),
+    document.write('<li><a href="/contactus/">Find a Center</a></li>\n'),
+    document.write('<li><a href="/partner/">Become a Franchisee</a></li>\n'),
+    document.write('<li><a href="/aboutus/careers/">Become an Employee</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-6 col-sm-3 col-md-3">\n'),
+    document.write("<h2>Know Us</h2>\n"),
+    document.write("\n"),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/aboutus/">Overview</a></li>\n'),
+    document.write('<li><a href="/aboutus/board-of-directors.html">Board of Directors</a></li>\n'),
+    document.write('<li><a href="/aboutus/management-team.html">Management Team</a></li>\n'),
+    document.write('<li><a href="/aboutus/media.html">Media Coverage</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-6 col-sm-3 col-md-2">\n'),
+    document.write("<h2>Go Social</h2>\n"),
+    document.write("\n"),
+    document.write("<ul>\n"),
+    document.write('<li><a href="//www.facebook.com/CLRocks">Facebook</a></li>\n'),
+    document.write('<li><a href="//www.instagram.com/career_launcher/">Instagram</a></li>\n'),
+    document.write('<li><a href="//www.linkedin.com/company/career-launcher/">Linkedin</a></li>\n'),
+    document.write('<li><a href="https://www.quora.com/profile/Career-Launcher-8">Quora</a></li>\n'),
+    document.write('<li><a href="//twitter.com/careerlauncher">Twitter</a></li>\n'),
+    document.write('<li><a href="//www.youtube.com/user/CareerLauncherMedia">YouTube</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-6 col-sm-3 col-md-2">\n'),
+    document.write("<h2>Enroll Now</h2>\n"),
+    document.write("\n"),
+    document.write("<ul>\n"),
+    document.write('\x3c!--<li><a href="/cl-online/">Buy Now</a></li>--\x3e\n'),
+    document.write('<li><a href="/sis/">Log-In</a></li>\n'),
+    document.write('<li><a href="https://myzone.careerlauncher.com">Sign-Up</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("\n"),
+    document.write('<ul class="twocolumn">\n'),
+    document.write("<li>\n"),
+    document.write('<a href="https://play.google.com/store/apps/details?id=com.careerlauncher.ai">\t\t\n'),
+    document.write('<img src="/images/android.png">\n'),
+    document.write("</a>\n"),
+    document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write('<a href="https://itunes.apple.com/us/app/cl-exam-guide/id1080624590?ls=1&mt=8">\t\t\n'),
+    document.write('<img src="//www.careerlauncher.com/images/app-store.png">\n'),
+    document.write("</a>\n"),
+    document.write("</li>\n"),
+    document.write("</ul>\n"),
+    document.write("\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</section>\n"),
+    document.write("\n"),
+    document.write('<section class="logoregionco text-center">\n'),
+    document.write('<div class="container">\n'),
+    document.write('<div class="row">\n'),
+    document.write('<img src="/images/logo-white.png" alt="CL">\n'),
+    document.write("\n"),
+    document.write("<ul>\n"),
+    document.write('<li><a href="/">India</a></li>\n'),
+    document.write('<li><a href="http://www.careerlauncheruae.com/">Dubai</a></li>\n'),
+    document.write('<li><a href="http://sg.careerlauncher.com">Singapore</a></li>\n'),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</section>\n"),
+    document.write("\n"),
+    document.write('<section class="companypartreg">\n'),
+    document.write('<div class="container">\n'),
+    document.write('<div class="row">\n'),
+    document.write('<div class="col-xs-12">\n'),
+    document.write("<h2>Business Verticals</h2>\n"),
+    document.write("<ul>\n"),
+    document.write("<li>\n"),
+    document.write('<a href="http://www.gkpublications.com/">GK Publications<br> <span>Books & <br>Publication</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write('<a href="https://kestoneglobal.com/">Kestone<br> <span>Marketing<br>  Services</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write('<a href="http://wainconnect.com//">WAIN-Connect<br> <span>Academia-Industry<br>  Network</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("\n"),
+    // document.write("<li>\n"),
+    // document.write('<a href="http://accendere.co.in/">Accendere<br> <span>Collegiate<br>  Research</span></a>\n'),
+    // document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-12">\n'),
+    document.write("<h2>Academic Verticals</h2>\n"),
+    document.write("<ul>\n"),
+    document.write("<li>\n"),
+    document.write('<a href="//www.lawentrance.com/">Law Entrance<br> <span>Coaching for  <br>CLAT AILET LSAT</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write('<a href="https://www.careerlauncher.com/gate/">GATE Entrance<br> <span>Coaching for <br>  GATE IES PSU</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("\n"),
+    document.write("<li>\n"),
+    document.write('<a href="http://futuremap.in/">Futuremap <br> <span>Career<br> Guidance</span></a>\n'),
+    document.write("</li>\n"),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("\n"),
+    document.write('<div class="col-xs-12 privacyregi">\n'),
+    document.write('<ul class="privcno">\n'),
+    document.write('<li><a href="/privacy.html">Privacy Policy & Terms of Sale</a></li>\n'),
+    document.write('<li><i class="fa fa-phone"></i>  8130-038-836 </li>\n'),
+    document.write("<li>&copy; CL Educate Ltd</li>\n"),
+    document.write("</ul>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</div>\n"),
+    document.write("</section>\n"),
+    document.write("</div>");
+
+$(document).ready(function () {
+    $("body").find(".starratingpage").parent("body").css({ "padding-bottom": "86px" });
+    $(".baner-region .col-md-4 iframe").removeAttr("scrolling");
+
+    /*(function (w, d, s, u) {
+w.Verloop = function (c) { w.Verloop._.push(c) }; w.Verloop._ = []; w.Verloop.url = u;
+var h = d.getElementsByTagName(s)[0], j = d.createElement(s); j.async = true;
+j.src = 'https://careerlauncher.verloop.io/livechat/script.min.js';
+h.parentNode.insertBefore(j, h);
+})(window, document, 'script', 'https://careerlauncher.verloop.io/livechat');
+Verloop(function() {    this.setCustomField("key", "value", { scope: "user" });});
+Verloop(function() {
+this.setRecipe("XYzAcH2LtMj7REwYB");
+});*/
+
+/* Latest script for Verllop Chatbot*/
+
+ (function (w, d, s, u) {
+        w.Verloop = function (c) {
+            w.Verloop._.push(c);
+        };
+        w.Verloop._ = [];
+        w.Verloop.url = u;
+        var h = d.getElementsByTagName(s)[0],
+            j = d.createElement(s);
+        j.async = true;
+        j.src = "https://careerlauncher.verloop.io/livechat/script.min.js";
+        h.parentNode.insertBefore(j, h);
+    })(window, document, "script", "https://careerlauncher.verloop.io/livechat");
+});
+
+// ********************Right hand side banner for cbse-ncert pages*********************
+
+var _url = window.location.href;
+var _uri = _url.split("/");
+var _filename = "";
+var _container = $("#font-cbse>.container .col-md-9");
+// $('.navbar-nav.navbar-right').remove();
+var cbseBannerData = "";
+
+if (_url.includes("class-8") || _url.includes("class-9") || _url.includes("class-10") || _url.includes("class-12"))
+    cbseBannerData = [
+        { imageUrl: "https://www.careerlauncher.com/images/FREE-Webinars-Ace-UG-Entrances-500x500.jpg", title: "CUET Free Webinars", btnText: "Know More", btnTargetUrl: "https://bit.ly/3v4G3QH" },
+        { imageUrl: "https://www.careerlauncher.com/images/Free-Zone-1200x1200.png", title: "CUET Freezone", btnText: "Know More", btnTargetUrl: "https://bit.ly/41iJTlg" },
+        { imageUrl: "https://www.careerlauncher.com/images/CUET-Sample-Paper-1200x1200.png", title: "CUET Sample paper", btnText: "Know More", btnTargetUrl: "https://bit.ly/3uYjCwN" },
+        { imageUrl: "https://www.careerlauncher.com/images/CUET-Readiness-Test-1200x1200.png", title: "CUET Readiness Test", btnText: "Know More", btnTargetUrl: "https://bit.ly/47TrCxD" },
+    ];
+
+var rnIndex = 0;
+
+rnIndex = Math.floor(Math.random() * cbseBannerData.length);
+var cbseBanner = cbseBannerData[rnIndex];
+
+var cbseBannerHtml = "";
+if (cbseBanner) {
+    cbseBannerHtml += '<div class="adds"><a href="' + cbseBanner.btnTargetUrl + '"><img src="' + cbseBanner.imageUrl + '" class="img-responsive"></a><span></span><div class="bnr-ads"><h2>' + cbseBanner.title + "</h2>";
+    var cbseBannerFeatures = false;
+    if (cbseBannerFeatures) {
+        cbseBannerHtml += "<ul>";
+        for (f = 0; f < cbseBannerFeatures.length; f++) {
+            cbseBannerHtml += "<li>" + cbseBannerFeatures[f] + "</li>";
+        }
+        cbseBannerHtml += "</ul>";
+    }
+    cbseBannerHtml += '</div><div class="bnr-btm"><a href="' + cbseBanner.btnTargetUrl + '" class="btn btn-same">' + cbseBanner.btnText + "</a></div></div>";
+    if (_url.includes("class-9") || _url.includes("class-10") || _url.includes("class-8") || _url.includes("class-12")) {
+        if (_container.siblings(".col-md-3").length == 0) {
+            _container.after('<div class="col-md-3 right-band">' + cbseBannerHtml + "</div>");
+        } else {
+            if (!_container.siblings(".col-md-3").hasClass("rigth-band")) {
+                _container.siblings(".col-md-3").append("");
+            }
+            _container.siblings(".col-md-3").append(cbseBannerHtml);
+            if (_uri[_uri.length - 1].indexOf("-") > -1) {
+                _container.siblings(".col-md-3").append(cbseBannerHtml);
+            } else {
+                _container.siblings(".col-md-3").append(cbseBannerHtml);
+            }
+            _container.parent().find('[href*="/counselling"]').parent().remove();
+        }
+        _container.siblings(".col-md-3").append($(".topics-container").removeClass("hide"));
+    }
+}
